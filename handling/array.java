8c2a18f46d50c63ed05ffab6f57e9a8c903bde63
sameer <sameergirni9@gmail.com>
2024-08-23 16:20:58 +0530

    hamdling

---
+++ handling/array.java
@@ -0,0 +1,17 @@
+package handling;
+
+public class array {
+
+	public static void main(String[] args) {
+		String[] name = {"sameer", "raju", "ram"};
+		System.out.println(name[0]);
+		System.out.println(name[1]);
+		System.out.println(name[2]);
+		
+		
+		
+		
+
+	}
+
+}
